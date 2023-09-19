5b53abe3741add56d3a3bb5fa6f26ec3a7c838c5
Jony Melo <jony.everton-@hotmail.com>
2023-09-19 12:01:36 -0300

    resolvendo alguns desafios

---
+++ src/DesafioStreamAPI/NumerosPositivos.java
@@ -0,0 +1,13 @@
+package DesafioStreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NumerosPositivos {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> numerosPositivos = numeros.stream().filter(n -> n > 0).toList();
+        System.out.println(numerosPositivos);
+    }
+}
---
+++ src/DesafioStreamAPI/SomaNumerosPar.java
@@ -0,0 +1,16 @@
+package DesafioStreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class SomaNumerosPar {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int sum = numeros.stream().filter(i -> i % 2 == 0).reduce(0, Integer::sum);
+        System.out.println(sum);
+
+    }
+}
